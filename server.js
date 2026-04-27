8a11616428626ed0dfdcde3596da704eb5016887
ug25ece013-arch <ug25ece013@iiitsurat.ac.in>
2026-04-27 10:13:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
-app.use(express.static(path.join(__dirname, "../")));
+app.use(express.static(path.join(__dirname, "index.html")));
 
 app.use("/analyze", analyzeRoute);
 app.use("/humanize", humanizeRoute);
